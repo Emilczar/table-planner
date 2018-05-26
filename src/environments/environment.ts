ad18ef30f93fc16484030d72f7b82f6dbdb9a0cf
Emilczar <emilczar@gmail.com>
2018-05-26 19:02:46 +0200

    add new project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-export const environment = {
-  production: false
-};
+  export const environment = {
+    production: false,
+    firebase: {
+    apiKey: 'AIzaSyCpuXdG634ej-Gj5CU7thj0NrZtF0jv588',
+    authDomain: 'plan-c2e05.firebaseapp.com',
+    databaseURL: 'https://plan-c2e05.firebaseio.com',
+    projectId: 'plan-c2e05',
+    storageBucket: 'plan-c2e05.appspot.com',
+    messagingSenderId: '649161435249'
+
+    }
+  };
